5c1abc2e985ec46cc16fdea829c4559c8116b052
Emmanuel Chimelokam <williamstonyblogspot@gmail.com>
2023-09-11 17:57:26 +0100

    First C code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - This is the entry point of the program 
+*
+*Return - returns zero if no errors otherwise returns non-zero value
+*
+*/
+
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+}
