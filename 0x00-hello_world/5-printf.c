2553b2a786eb1b075fea74de20ccc19ab16f776d
Emmanuel Chimelokam <williamstonyblogspot@gmail.com>
2023-09-12 04:35:47 +0100

    Second C code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - This is the entry point of the program
+*
+*Return: returns zero if no errors otherwise returns non-zero value
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
